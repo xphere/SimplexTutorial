0a5cf19c3f16eaa0312aba31c55efa341f3fb5b7
Berny Cantos <berny@newvisiolution.com>
2012-08-10 16:45:41 +0200

    Refactor common controller code to init.php

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
 <?php
 
-require_once __DIR__ . '/autoload.php';
+require_once __DIR__ . '/init.php';
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-
-$request = Request::createFromGlobals();
 $input = $request->get('name', 'World');
-$response = new Response(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
+
+$response->setContent(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
 $response->send();
