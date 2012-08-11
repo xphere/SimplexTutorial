3db399d6d7ea57512f00e1bc3c300bfad36a55d9
Berny Cantos <xphere81@gmail.com>
2012-08-11 15:15:18 +0200

    Use Routing component in front controller

--- web/front.php
+++ web/front.php
@@ -4,24 +4,34 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+
+$routes = new RouteCollection();
+$routes->add('hello', new Route('/hello/{name}', array('name' => 'World')));
+$routes->add('bye', new Route('/bye'));
 
 $request = Request::createFromGlobals();
-$response = new Response();
 
-$map = array(
-    '/hello' => '/hello.php',
-    '/bye'   => '/bye.php',
-);
+$context = new RequestContext();
+$context->fromRequest($request);
 
-$path = $request->getPathInfo();
-if (isset($map[$path])) {
+$matcher = new UrlMatcher($routes, $context);
+
+try {
+    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
     ob_start();
-    extract($request->query->all(), EXTR_SKIP);
-    require __DIR__ . '/../src/pages' . $map[$path];
-    $response->setContent(ob_get_clean());
-} else {
-    $response->setStatusCode(404);
-    $response->setContent('Not Found');
+    require __DIR__ . '/../src/pages/' . $_route . '.php';
+    $response = new Response(ob_get_clean());
+
+} catch (ResourceNotFoundException $e) {
+    $response = new Response('Not Found', 404);
+
+} catch (Exception $e) {
+    $response = new Response('An error occurred', 500);
 }
 
 $response->send();
