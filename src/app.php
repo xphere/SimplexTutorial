516eda6b2f10ff87ae683169acfa4b0452154fc1
Berny Cantos <xphere81@gmail.com>
2012-08-11 15:37:34 +0200

    Introducing is_leap_year

--- src/app.php
+++ src/app.php
@@ -1,10 +1,27 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
 
 $routes = new RouteCollection();
 $routes->add('hello', new Route('/hello/{name}', array('name' => 'World')));
 $routes->add('bye', new Route('/bye'));
+
+function is_leap_year($year)
+{
+    return $year % 400 === 0 || ($year % 4 === 0 && $year % 100 !== 0);
+}
+
+$routes->add('leap_year', new Route('/is_leap_year/{year}', array(
+    'year' => null,
+    '_controller' => function ($request) {
+        $year = $request->attributes->get('year') ?: date('Y');
+        if (is_leap_year($year)) {
+            return new Response('Yep, this is a leap year!');
+        }
+        return new Response('Nope, this is not a leap year.');
+    }
+)));
 
 return $routes;
