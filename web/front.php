816ad8c6a09162f61e9e76b50c10b4dba4d595f5
Berny Cantos <xphere81@gmail.com>
2012-08-11 15:06:38 +0200

    Use composer autoload

--- web/front.php
+++ web/front.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/../src/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
