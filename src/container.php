e5bbc858cb158cfe3782f0c9e5fa676f238a21a8
Berny Cantos <berny@newvisiolution.com>
2012-08-14 16:30:30 +0200

    Move all wires that form our framework into a container

---
+++ src/container.php
@@ -0,0 +1,35 @@
+<?php
+
+use Symfony\Component\DependencyInjection;
+use Symfony\Component\DependencyInjection\Reference;
+
+$dic = new DependencyInjection\ContainerBuilder();
+
+$dic->register('context', 'Symfony\Component\Routing\RequestContext');
+
+$dic->register('matcher', 'Symfony\Component\Routing\Matcher\UrlMatcher')
+    ->setArguments(array($routes, new Reference('context')));
+
+$dic->register('resolver', 'Symfony\Component\HttpKernel\Controller\ControllerResolver');
+ 
+$dic->register('listener.router', 'Symfony\Component\HttpKernel\EventListener\RouterListener')
+    ->setArguments(array(new Reference('matcher')));
+
+$dic->register('listener.response', 'Symfony\Component\HttpKernel\EventListener\ResponseListener')
+    ->setArguments(array('UTF-8'));
+
+$dic->register('listener.content_length', 'Simplex\Event\ContentLengthListener');
+$dic->register('listener.string_response', 'Simplex\Event\StringResponseListener');
+
+$dic->register('dispatcher', 'Symfony\Component\EventDispatcher\EventDispatcher')
+    ->addMethodCall('addSubscriber', array(new Reference('listener.router')))
+    ->addMethodCall('addSubscriber', array(new Reference('listener.response')))
+    ->addMethodCall('addSubscriber', array(new Reference('listener.exception')))
+    ->addMethodCall('addSubscriber', array(new Reference('listener.content_length')))
+    ->addMethodCall('addSubscriber', array(new Reference('listener.string_response')))
+;
+
+$dic->register('framework', 'Simplex\Framework')
+    ->setArguments(array(new Reference('dispatcher'), new Reference('resolver')));
+
+return $dic;
--- web/front.php
+++ web/front.php
@@ -3,29 +3,11 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing;
-use Symfony\Component\HttpKernel;
-use Symfony\Component\EventDispatcher\EventDispatcher;
-use Simplex\Event\ContentLengthListener;
-use Simplex\Event\StringResponseListener;
-use Calendar\Event\LegalListener;
 
 $request = Request::createFromGlobals();
 $routes = require __DIR__ . '/../src/app.php';
+$dic = require __DIR__ . '/../src/container.php';
 
-$context = new Routing\RequestContext();
-$context->fromRequest($request);
-$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
-$resolver = new HttpKernel\Controller\ControllerResolver();
-
-$dispatcher = new EventDispatcher();
-$dispatcher->addSubscriber(new HttpKernel\EventListener\RouterListener($matcher));
-$dispatcher->addSubscriber(new HttpKernel\EventListener\ResponseListener('UTF-8'));
-$dispatcher->addSubscriber(new LegalListener());
-$dispatcher->addSubscriber(new ContentLengthListener());
-$dispatcher->addSubscriber(new StringResponseListener());
-
-$framework = new Simplex\Framework($dispatcher, $resolver);
-$response = $framework->handle($request);
-
-$response->send();
+$dic->get('framework')
+    ->handle($request)
+    ->send();
