b0d72315e13a8915b2ca01cf54f360acd318c6c5
Berny Cantos <berny@newvisiolution.com>
2012-08-14 16:32:27 +0200

    Add app centered listeners from front controller

--- web/front.php
+++ web/front.php
@@ -3,10 +3,16 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\DependencyInjection\Reference;
 
 $request = Request::createFromGlobals();
 $routes = require __DIR__ . '/../src/app.php';
 $dic = require __DIR__ . '/../src/container.php';
+
+$dic->register('listener.calendar_legal', 'Calendar\Event\LegalListener');
+$dic->getDefinition('dispatcher')
+    ->addMethodCall('addSubscriber', array(new Reference('listener.calendar_legal')))
+;
 
 $dic->get('framework')
     ->handle($request)
