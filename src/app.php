3fb752511852b649d2616f90f6261f0befb7ccdc
Berny Cantos <berny@newvisiolution.com>
2012-08-14 09:34:11 +0200

    Front controller moved to own class

--- src/app.php
+++ src/app.php
@@ -6,8 +6,6 @@ use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
 
 $routes = new RouteCollection();
-$routes->add('hello', new Route('/hello/{name}', array('name' => 'World')));
-$routes->add('bye', new Route('/bye'));
 $routes->add('leap_year', new Route('/is_leap_year/{year}', array(
     'year' => null,
     '_controller' => 'LeapYearController::indexAction',
